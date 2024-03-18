9361e5b6f2340939b027dd810458ceef84bf45fe
kristinaarezina <kristina.arezina.dev>
2024-03-18 14:52:49 -0400

    add

---
+++ application.js
@@ -0,0 +1,107 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const connectionToPg = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
+const inputAndOutput = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const askingForInput = () => {
+  console.log(`Enter a number to run the following: \n 1. Get all students \n 2. Add student \n 3. Update student \n 4. Delete a student \n 5. Bye!! `);
+  inputAndOutput.question('Number: ', async (answer) => {
+    if (answer.length > 0) {
+        if (answer === "1") {
+            await getAllStudents();
+            askingForInput();
+        } else if (answer === "2") {
+            await addStudent("Kristina", "Arezina", "arezina.kristina23@gmail.com", "2024-03-17");
+            askingForInput();
+        } else if (answer === "3") {
+            await updateStudentEmail(1, "newemail@gmail.com");
+            askingForInput();
+        } else if (answer === "4") {
+            await deleteStudent(1);
+            askingForInput();
+        } else if (answer === "5") {
+            console.log("Okay bye!");
+            inputAndOutput.close();
+            connectionToPg.end();
+        } else {
+            console.log("Invalid input, try again.");
+            askingForInput();
+        }
+    } else {
+        console.log("Okay bye!");
+        inputAndOutput.close();
+        connectionToPg.end();
+    }
+  });
+};
+
+const getAllStudents = () => {
+    return new Promise((resolve, reject) => {
+        connectionToPg.query('SELECT * FROM students', (err, res) => {
+            if (err) {
+                console.error('Error executing query', err.stack);
+                reject(err);
+            } else {
+                console.log(res.rows);
+                resolve();
+            }
+        });
+    });
+};
+
+const addStudent = (first_name, last_name, email, enrollment_date) => {
+    return new Promise((resolve, reject) => {
+        const queryText = 'INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES ($1, $2, $3, $4) RETURNING *;';
+        const values = [first_name, last_name, email, enrollment_date];
+        connectionToPg.query(queryText, values, (err, res) => {
+            if (err) {
+                console.error('Error executing query', err.stack);
+                reject(err);
+            } else {
+                console.log(res.rows);
+                resolve();
+            }
+        });
+    });
+};
+
+const updateStudentEmail = (student_id, new_email) => {
+    return new Promise((resolve, reject) => {
+        const queryText = 'UPDATE students SET email = $2 WHERE student_id = $1 RETURNING *;';
+        const values = [student_id, new_email];
+        connectionToPg.query(queryText, values, (err, res) => {
+            if (err) {
+                console.error('Error executing query', err.stack);
+                reject(err);
+            } else {
+                console.log(res.rows);
+                resolve();
+            }
+        });
+    });
+};
+
+const deleteStudent = (student_id) => {
+    return new Promise((resolve, reject) => {
+        const queryText = 'DELETE FROM students WHERE student_id = $1 RETURNING *;';
+        const values = [student_id];
+        connectionToPg.query(queryText, values, (err, res) => {
+            if (err) {
+                console.error('Error executing query', err.stack);
+                reject(err);
+            } else {
+                console.log(res.rows);
+                resolve();
+            }
+        });
+    });
+};
+
+askingForInput(); // Start the interaction
